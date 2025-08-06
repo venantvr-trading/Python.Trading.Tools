9f3f8fd72185bb1f401f077c6521aa367963ff53
rvv <r.venant.valery@gmail.com>
2025-08-06 13:19:45 +0200

    First commit...

---
+++ business/tools/__init__.py
@@ -0,0 +1,14 @@
+from .caching import cache_to_pickle, cache_to_json, cache_for_n_calls
+from .logger import setup_logging, get_formatter, configure_stream, logger
+from .stream import StreamToLogger
+
+__all__ = [
+    "cache_to_pickle",
+    "cache_to_json",
+    "cache_for_n_calls",
+    "setup_logging",
+    "get_formatter",
+    "configure_stream",
+    "logger",
+    "StreamToLogger",
+]
