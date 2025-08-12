c06a01988c866cacd9e67b36549551650f9f61a8
rvv <r.venant.valery@gmail.com>
2025-08-12 18:36:36 +0200

    First commit...

--- venantvr/tools/caching.py
+++ venantvr/tools/caching.py
@@ -1,8 +1,98 @@
 import functools
 
 import json
+import os
 import pickle
 from functools import wraps
+from pathlib import Path
+
+
+def dynamic_cache_to_json(template_dir):
+    """
+    Un décorateur qui génère un chemin de fichier de cache dynamique
+    basé sur les attributs de l'instance de la classe et crée l'arborescence si nécessaire.
+    """
+
+    """
+    Décorateur générique pour cacher le résultat d'une fonction dans un fichier JSON.
+    Le chemin de cache est construit à partir d'un template de répertoire et des propriétés de l'instance.
+
+    :param template_dir: Un chemin de répertoire de cache avec des placeholders (ex: 'cache/{exchange_name}/').
+                         Le nom du fichier est implicitement le nom de la fonction décorée.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            # Formater le chemin du répertoire avec toutes les propriétés de l'instance
+            cache_dir = Path(template_dir.format(**self.__dict__))
+
+            # Utiliser le nom de la fonction pour le nom du fichier
+            cache_filename = f"{func.__name__}.json"
+            cache_path = cache_dir / cache_filename
+
+            # Créer l'arborescence des dossiers si elle n'existe pas
+            os.makedirs(cache_dir, exist_ok=True)
+
+            # Logique de chargement du cache
+            if os.path.exists(cache_path):
+                with open(cache_path, 'r') as file:
+                    return json.load(file)
+
+            # Exécuter la fonction d'origine
+            result = func(self, *args, **kwargs)
+
+            # Logique de sauvegarde du cache
+            with open(cache_path, 'w') as file:
+                json.dump(result, file)
+
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+def dynamic_cache_to_pickle(template_dir):
+    """
+    Décorateur générique pour cacher le résultat d'une fonction dans un fichier pickle.
+    Le chemin de cache est construit à partir d'un template de répertoire et des propriétés de l'instance.
+
+    :param template_dir: Un chemin de répertoire de cache avec des placeholders (ex: 'cache/{exchange_name}/').
+                         Le nom du fichier est implicitement le nom de la fonction décorée.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            # Formater le chemin du répertoire avec toutes les propriétés de l'instance
+            cache_dir = Path(template_dir.format(**self.__dict__))
+
+            # Utiliser le nom de la fonction pour le nom du fichier
+            cache_filename = f"{func.__name__}.pickle"
+            cache_path = cache_dir / cache_filename
+
+            # Créer l'arborescence des dossiers si elle n'existe pas
+            os.makedirs(cache_dir, exist_ok=True)
+
+            # Logique de chargement du cache
+            if os.path.exists(cache_path):
+                with open(cache_path, 'rb') as file:
+                    return pickle.load(file)
+
+            # Exécuter la fonction d'origine
+            result = func(self, *args, **kwargs)
+
+            # Logique de sauvegarde du cache
+            with open(cache_path, 'wb') as file:
+                # noinspection PyTypeChecker
+                pickle.dump(result, file)
+
+            return result
+
+        return wrapper
+
+    return decorator
 
 
 def cache_to_pickle(path):
