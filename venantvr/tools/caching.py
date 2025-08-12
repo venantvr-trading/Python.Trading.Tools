5351afe0946d6f1d35814db3659de6bc479079ef
rvv <r.venant.valery@gmail.com>
2025-08-12 21:22:38 +0200

    First commit...

--- venantvr/tools/caching.py
+++ venantvr/tools/caching.py
@@ -1,5 +1,3 @@
-import functools
-
 import json
 import os
 import pickle
@@ -7,29 +5,23 @@ from functools import wraps
 from pathlib import Path
 
 
-def dynamic_cache_to_json(template_dir):
-    """
-    Un décorateur qui génère un chemin de fichier de cache dynamique
-    basé sur les attributs de l'instance de la classe et crée l'arborescence si nécessaire.
-    """
-
+def dynamic_cache_to_json(template_dir, cache_filename=None):
     """
     Décorateur générique pour cacher le résultat d'une fonction dans un fichier JSON.
     Le chemin de cache est construit à partir d'un template de répertoire et des propriétés de l'instance.
 
     :param template_dir: Un chemin de répertoire de cache avec des placeholders (ex: 'cache/{exchange_name}/').
-                         Le nom du fichier est implicitement le nom de la fonction décorée.
+    :param cache_filename: (Optionnel) Nom du fichier de cache. Par défaut, c'est le nom de la fonction décorée avec l'extension .json.
     """
-
     def decorator(func):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
             # Formater le chemin du répertoire avec toutes les propriétés de l'instance
             cache_dir = Path(template_dir.format(**self.__dict__))
 
-            # Utiliser le nom de la fonction pour le nom du fichier
-            cache_filename = f"{func.__name__}.json"
-            cache_path = cache_dir / cache_filename
+            # Utiliser le nom de fichier optionnel, sinon le nom de la fonction
+            final_cache_filename = cache_filename if cache_filename else f"{func.__name__}.json"
+            cache_path = cache_dir / final_cache_filename
 
             # Créer l'arborescence des dossiers si elle n'existe pas
             os.makedirs(cache_dir, exist_ok=True)
@@ -37,14 +29,17 @@ def dynamic_cache_to_json(template_dir):
             # Logique de chargement du cache
             if os.path.exists(cache_path):
                 with open(cache_path, 'r') as file:
-                    return json.load(file)
+                    cached_data = json.load(file)
+                    # Retourner l'objet mis en cache
+                    return cached_data.get("value")
 
             # Exécuter la fonction d'origine
             result = func(self, *args, **kwargs)
 
             # Logique de sauvegarde du cache
             with open(cache_path, 'w') as file:
-                json.dump(result, file)
+                # Encapsuler le résultat dans un dictionnaire avec la clé "value"
+                json.dump({"value": result}, file)
 
             return result
 
@@ -53,13 +48,13 @@ def dynamic_cache_to_json(template_dir):
     return decorator
 
 
-def dynamic_cache_to_pickle(template_dir):
+def dynamic_cache_to_pickle(template_dir, cache_filename=None):
     """
     Décorateur générique pour cacher le résultat d'une fonction dans un fichier pickle.
     Le chemin de cache est construit à partir d'un template de répertoire et des propriétés de l'instance.
 
     :param template_dir: Un chemin de répertoire de cache avec des placeholders (ex: 'cache/{exchange_name}/').
-                         Le nom du fichier est implicitement le nom de la fonction décorée.
+    :param cache_filename: (Optionnel) Nom du fichier de cache. Par défaut, c'est le nom de la fonction décorée avec l'extension .pickle.
     """
 
     def decorator(func):
@@ -68,9 +63,9 @@ def dynamic_cache_to_pickle(template_dir):
             # Formater le chemin du répertoire avec toutes les propriétés de l'instance
             cache_dir = Path(template_dir.format(**self.__dict__))
 
-            # Utiliser le nom de la fonction pour le nom du fichier
-            cache_filename = f"{func.__name__}.pickle"
-            cache_path = cache_dir / cache_filename
+            # Utiliser le nom de fichier optionnel, sinon le nom de la fonction
+            final_cache_filename = cache_filename if cache_filename else f"{func.__name__}.pickle"
+            cache_path = cache_dir / final_cache_filename
 
             # Créer l'arborescence des dossiers si elle n'existe pas
             os.makedirs(cache_dir, exist_ok=True)
@@ -78,94 +73,20 @@ def dynamic_cache_to_pickle(template_dir):
             # Logique de chargement du cache
             if os.path.exists(cache_path):
                 with open(cache_path, 'rb') as file:
-                    return pickle.load(file)
+                    cached_data = pickle.load(file)
+                    # Retourner l'objet mis en cache
+                    return cached_data.get("value")
 
             # Exécuter la fonction d'origine
             result = func(self, *args, **kwargs)
 
             # Logique de sauvegarde du cache
             with open(cache_path, 'wb') as file:
+                # Encapsuler le résultat dans un dictionnaire avec la clé "value"
                 # noinspection PyTypeChecker
-                pickle.dump(result, file)
+                pickle.dump({"value": result}, file)
 
             return result
-
-        return wrapper
-
-    return decorator
-
-
-def cache_to_pickle(path):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            try:
-                with open(path, 'rb') as pickle_file:
-                    cached_result = pickle.load(pickle_file)
-            except FileNotFoundError:
-                cached_result = None
-            if cached_result is not None:
-                return cached_result
-            else:
-                result = func(*args, **kwargs)
-                with open(path, 'wb') as pickle_file:
-                    # noinspection PyTypeChecker
-                    pickle.dump(result, pickle_file)
-                return result
-
-        return wrapper
-
-    return decorator
-
-
-def cache_to_json(path):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            try:
-                with open(path, 'r') as json_file:
-                    cached_result = json.load(json_file)
-            except FileNotFoundError:
-                cached_result = None
-            if cached_result is not None:
-                return cached_result
-            else:
-                result = func(*args, **kwargs)
-                with open(path, 'w') as json_file:
-                    # noinspection PyTypeChecker
-                    json.dump(result, json_file, indent=2)
-                return result
-
-        return wrapper
-
-    return decorator
-
-
-def cache_for_n_calls(n):
-    """
-    Décorateur pour cacher le résultat d'une méthode pendant 'n' appels,
-    avec un mécanisme de désactivation externe.
-
-    :param n: Nombre d'appels pendant lequel le cache est actif.
-    """
-
-    def decorator(func):
-        cached_result = None  # Pour stocker le résultat de la méthode
-        call_count = 0  # Compteur d'appels
-
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            nonlocal call_count, cached_result
-
-            # Si le compteur est en dessous de 'n', retourner le résultat mis en cache
-            if call_count % n != 0:
-                call_count += 1
-                return cached_result
-
-            # Sinon, réinitialiser le compteur et recalculer le résultat
-            call_count = 1  # Réinitialisation après recalcul
-            cached_result = func(*args, **kwargs)
-            return cached_result
 
         return wrapper
 
